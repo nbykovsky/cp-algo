0ab0abe996e50ca3f84343ace236ce0d513fa1b5
Nikolay <nikolay.bykovsky@gmail.com>
2019-11-27 23:16:46 +1100

    started avl tree

--- src/main/avl_tree.py
+++ src/main/avl_tree.py
@@ -5,6 +5,7 @@ class TreeNode:
         self.value = v
         self.left = None
         self.right = None
+        self.parent = None
         self.balance_factor = 0
 
 
@@ -14,6 +15,32 @@ class AvlTree:
         self._tree = TreeNode(k, v)
 
     def add(self, k, v):
+        node = self._add(k, v)
+        if node:
+            self._rebalance(node)
+
+    def _add(self, k, v):
+        node = self._tree
+        while node:
+            if k < node.key:
+                if node.left:
+                    node = node.left
+                else:
+                    node.left = TreeNode(k, v)
+                    node.left.parent = node
+                    return node.left
+            elif node.key < k:
+                if node.right:
+                    node = node.right
+                else:
+                    node.right = TreeNode(k, v)
+                    node.right.parent = node
+                    return node.right
+            else:
+                node.value = v
+                return
+
+    def _rebalance(self, node):
         pass
 
     def remove(self, k, v):
